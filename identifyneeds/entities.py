7c283bbddfdfcc1bcd210fb2ad1f6d07406fa5fd
Robin Staunton-Collins <robin.staunton-collins@capgemini.com>
2019-12-24 20:56:09 +0000

    Added basics of Condition and Characteristic

--- identifyneeds/entities.py
+++ identifyneeds/entities.py
@@ -1,11 +1,45 @@
+from typing import Dict
+
 
 class Characteristic():
 
-    def __init__(self, uuid: int, text: str, category: str):
-        self.uuid = uuid,
-        self.text = text,
+    def __init__(
+            self,
+            uuid: str = None,
+            text: str = None,
+            category: str = None,
+            condition_weightings: Dict[str, int] = None):
+        self.uuid = uuid
+        self.text = text
         self.category = category
+        self.condition_weightings = condition_weightings
+
+    def __str__(self):
+        return f"{self.text} - Category: {self.category} - Associated Conditions: {self.condition_weightings.keys()}"
+
+    def __repr__(self):
+        return f"{self.text} - Category: {self.category} - Associated Conditions: {self.condition_weightings.keys()}"
+
+    def add_points_to_condition(self, condition, points):
+        if condition.name in self.condition_weightings.keys():
+            points = points * self.condition_weightings[condition.name]
+            condition.add_points(points)
 
 
 class Condition():
-    pass
+
+    def __init__(
+            self,
+            uuid: str = None,
+            name: str = None,
+            points: int = 0
+    ):
+        self.uuid = uuid
+        self.name = name
+        self.points = points
+
+    def __repr__(self):
+        return f"{self.name} - {self.points}"
+
+    def add_points(self, amount: int):
+        self.points += amount
--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,16 +1,89 @@
-from identifyneeds.entities import Characteristic
+from unittest.mock import Mock
+
+import pytest
+from identifyneeds.entities import Characteristic, Condition
 
 
 class TestCharacteristic():
 
-    def test_char_init(self):
-
-        char = Characteristic(
+    @pytest.fixture()
+    def disruptive_char(self):
+        return Characteristic(
             uuid=1,
             text="Disruptive in Class",
             category="Motivation and Concentration",
+            condition_weightings={
+                'Autism': 2,
+                'Aspergers': 1,
+                'Conduct Dis': 2,
+                'OOD': 2,
+                'Low IQ': 1,
+                'ADHD': 2,
+                'Tourettes': 2,
+                'Giftedness': 1,
+                'Abuse': 1
+            }
         )
-        assert char.uuid == 1
+
+    @pytest.fixture()
+    def mock_condition(self):
+        mock = Mock()
+        return mock
+
+    def test_char_init(self):
+
+        char = Characteristic(
+            uuid='char-1',
+            text="Disruptive in Class",
+            category="Motivation and Concentration",
+            condition_weightings={
+                'Autism': 2,
+                'Aspergers': 1,
+                'Conduct Dis': 2,
+                'OOD': 2,
+                'Low IQ': 1,
+                'ADHD': 2,
+                'Tourettes': 2,
+                'Giftedness': 1,
+                'Abuse': 1
+            }
+        )
+        assert char.uuid == 'char-1'
         assert char.text == "Disruptive in Class"
         assert char.category == "Motivation and Concentration"
+        assert char.condition_weightings == {
+            'Autism': 2,
+            'Aspergers': 1,
+            'Conduct Dis': 2,
+            'OOD': 2,
+            'Low IQ': 1,
+            'ADHD': 2,
+            'Tourettes': 2,
+            'Giftedness': 1,
+            'Abuse': 1
+        }
 
+    def test_add_points_to_condition(self, disruptive_char, mock_condition):
+        condition = mock_condition
+        condition.name = 'Autism'
+        disruptive_char.add_points_to_condition(condition, 2)
+        condition.add_points.assert_called_once_with(4)
+
+
+class TestCondition():
+
+    def test_condition_init(self):
+        cnd = Condition(
+            uuid='cnd-1',
+            name="Autism",
+            points=2
+        )
+
+        assert cnd.uuid == 'cnd-1'
+        assert cnd.name == 'Autism'
+        assert cnd.points == 2
+
+    def test_add_points(self):
+        cnd = Condition()
+        cnd.add_points(5)
+        assert cnd.points == 5
